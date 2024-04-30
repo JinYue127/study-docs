3d941eefcaf66af8d42fae9c2f1770a26e2b223b
金岳 <jinyue@lucidada.cn>
2024-04-30 17:19:03 +0800

    feat: 友情链接

--- .vitepress/config/nav.ts
+++ .vitepress/config/nav.ts
@@ -33,6 +33,9 @@ export const nav: DefaultTheme.Config['nav'] = [
   {
     text: '友情链接',
     items: [
+      {text: '前端文章', link: 'https://fed.chanceyu.com/'},
+      {text: '前端文章', link: 'https://fed.chanceyu.com/'},
+      {text: 'Next.js学习', link: 'https://www.codewithantonio.com/'},
       {text: 'Radash学习', link: 'https://jinyue12138.gitee.io/radash-study/'},
       {text: 'wormhole', link: 'https://wormhole.app/'},
       {text: '阿里小站', link: 'https://pan666.net/'},
